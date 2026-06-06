bcd1bb1fc1fc3e67040af85a9b06e03dc0be80f4
Alien Q <celestialq7@gmail.com>
2026-06-06 19:58:37 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import RoutesPage from "./pages/Routes";
+import Vehicles from "./pages/Vehicles";
+import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
+import Layout from "./components/Layout";
+import Payment from "./pages/Payment";
+import Ticket from "./pages/Ticket";
+import Scanner from "./pages/Scanner";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+            path="/payment/:id"
+            element={<Payment />}
+          />
+         <Route path="/ticket/:id" element={<Ticket />} />
+         
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/book" element={<Booking />} />
+
+        <Route path="/" element={<Layout />}>
+          <Route
+           path="/dashboard"
+           element={
+           <ProtectedRoute>
+           <Dashboard />
+           </ProtectedRoute>
+           }
+          />
+
+          <Route
+            path="routes"
+            element={
+			<ProtectedRoute>
+			<RoutesPage />
+			</ProtectedRoute>}
+          />
+
+          <Route
+            path="vehicles"
+            element={
+			<ProtectedRoute>
+			<Vehicles />
+			</ProtectedRoute>}
+          />
+
+          <Route
+  path="/bookings"
+  element={
+    <ProtectedRoute>
+      <Bookings />
+    </ProtectedRoute>
+  }
+/>
+          
+          <Route path="/scanner" element={<ProtectedRoute><Scanner /></ProtectedRoute>} />
+         </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
