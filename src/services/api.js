aaae1d7915ecf3b9dcec7679690dce038f42d88c
Alien Q <celestialq7@gmail.com>
2026-06-06 23:46:47 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,18 @@
-const API_URL = "http://localhost:8787";
+//export API_URL = "http://localhost:8787";
+//exportt async function apiFetch(url, options = {}) {
+//  const token = localStorage.getItem("token");
+/*
+  return fetch(`${API_URL}${url}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      ...(options.headers || {}),
+    },
+  });
+}*/
 
-export async function apiFetch(url, options = {}) {
+const API_URL = "https://transport-backend.pages.dev";returnort async function apiFetch(url, options = {}) {
   const token = localStorage.getItem("token");
 
   return fetch(`${API_URL}${url}`, {
