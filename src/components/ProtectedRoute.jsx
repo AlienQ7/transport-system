914dd6e54cd834baeabd8ff9d4bfa7a1ee251408
Alien Q <celestialq7@gmail.com>
2026-06-07 15:09:42 +0500

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+/*import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({
   children,
@@ -9,6 +9,23 @@ export default function ProtectedRoute({
   if (!token) {
   return <Navigate to="/login" replace />;
 }
+
+  return children;
+}
+*/
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children, allowedRoles }) {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    return <Navigate to="/dashboard" replace />;
+  }
 
   return children;
 }
