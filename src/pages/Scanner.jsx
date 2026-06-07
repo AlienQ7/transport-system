bfd3c39aee09d479378b987f6580bffedbab61be
Alien Q <celestialq7@gmail.com>
2026-06-07 10:44:16 +0500

    Update Scanner.jsx

--- src/pages/Scanner.jsx
+++ src/pages/Scanner.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+//import { useState } from "react";
+import { useState, useEffect } from "react";
 import { apiFetch } from "../services/api";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
