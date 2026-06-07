2b737caf72bc6bbab3fc5b1d6400bd16ab98d064
Alien Q <celestialq7@gmail.com>
2026-06-07 15:42:08 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,61 +12,69 @@ import Payment from "./pages/Payment";
 import Ticket from "./pages/Ticket";
 import Scanner from "./pages/Scanner";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+import Staff from "./pages/Staff";
+import Driver from "./pages/Driver";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* PUBLIC */}
         <Route path="/" element={<Home />} />
-        <Route
-            path="/payment/:id"
-            element={<Payment />}
-          />
-         <Route path="/ticket/:id" element={<Ticket />} />
-         
-
+        <Route path="/payment/:id" element={<Payment />} />
+        <Route path="/ticket/:id" element={<Ticket />} />
         <Route path="/login" element={<Login />} />
-
         <Route path="/book" element={<Booking />} />
 
+        {/* STAFF + DRIVER (now correctly inside Routes) */}
+        <Route path="/staff" element={
+          <ProtectedRoute>
+            <Staff />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/driver" element={
+          <ProtectedRoute>
+            <Driver />
+          </ProtectedRoute>
+        } />
+
+        {/* LAYOUT WRAPPED ROUTES */}
         <Route path="/" element={<Layout />}>
-          <Route
-           path="/dashboard"
-           element={
-           <ProtectedRoute>
-           <Dashboard />
-           </ProtectedRoute>
-           }
-          />
-
-          <Route
-            path="routes"
-            element={
-			<ProtectedRoute>
-			<RoutesPage />
-			</ProtectedRoute>}
-          />
-
-          <Route
-            path="vehicles"
-            element={
-			<ProtectedRoute>
-			<Vehicles />
-			</ProtectedRoute>}
-          />
-
-          <Route
-  path="/bookings"
-  element={
-    <ProtectedRoute>
-      <Bookings />
-    </ProtectedRoute>
-  }
-/>
           
-          <Route path="/scanner" element={<ProtectedRoute><Scanner /></ProtectedRoute>} />
-         </Route>
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route path="routes" element={
+            <ProtectedRoute>
+              <RoutesPage />
+            </ProtectedRoute>
+          } />
+
+          <Route path="vehicles" element={
+            <ProtectedRoute>
+              <Vehicles />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/bookings" element={
+            <ProtectedRoute>
+              <Bookings />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/scanner" element={
+            <ProtectedRoute>
+              <Scanner />
+            </ProtectedRoute>
+          } />
+
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
